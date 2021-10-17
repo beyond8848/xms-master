4292b5c2a7261d4d4919e9429b0a8de2c68548bb
beyond8848 <cplusplus@126.com>
2021-10-17 18:56:08 +0800

    加入Logservice接口

--- Presentation/Xms.Web/Api/DataArchiveController.cs
+++ Presentation/Xms.Web/Api/DataArchiveController.cs
@@ -10,7 +10,7 @@ using Xms.Web.Framework.Controller;
 using Xms.Sdk.Client;
 using Xms.Core.Data;
 using Xms.Web.Framework.Mvc;
-
+using Xms.Logging.AppLog;
 
 namespace Xms.Web.Api
 {
@@ -20,16 +20,19 @@ namespace Xms.Web.Api
     {
         private readonly IDataFinder _dataFinder;
         private readonly IDataUpdater _dataUpdater;
+        private readonly ILogService _logService;
 
         private readonly string ASIPFolderPoolDir = System.AppDomain.CurrentDomain.BaseDirectory+"ASIP_POOL_FILES";
         public DataArchiveController(IWebAppContext appContext
             , IDataFinder dataFinder
             , IDataUpdater dataUpdater
+            , ILogService logService
              )
             : base(appContext)
         {
             _dataFinder = dataFinder;
             _dataUpdater = dataUpdater;
+            _logService = logService;
         }
 
         [Description("预归档")]
@@ -39,24 +42,30 @@ namespace Xms.Web.Api
             bool processSussess = false;
             foreach(var id in reimbursmentIds)
             {
-                Guid reimbursmentId = new Guid(id);
-                Entity entity = _dataFinder.RetrieveById("Reimbursement", reimbursmentId);
-                if(entity!=null)
+                try
                 {
-                    string claimNo = entity.GetValueOrDefault("ClaimNo").ToString();
-                    if(!string.IsNullOrWhiteSpace(claimNo))
+                    Guid reimbursmentId = new Guid(id);
+                    Entity entity = _dataFinder.RetrieveById("Reimbursement", reimbursmentId);
+                    if (entity != null)
                     {
-                        string file = ASIPFolderPoolDir + "\\" + claimNo + ".zzip";
-                        string destFile = ASIPFolderPoolDir + "\\" + claimNo + ".zip";
-                        if (System.IO.File.Exists(file))
+                        string claimNo = entity.GetValueOrDefault("ClaimNo").ToString();
+                        if (!string.IsNullOrWhiteSpace(claimNo))
                         {
-                            System.IO.File.Move(file, destFile);
-                            Core.Data.Entity updateEntity = new Core.Data.Entity("Reimbursement");
-                            updateEntity.SetAttributeValue("IsArchived", 1);
-                            updateEntity.SetIdValue(reimbursmentId);
-                            _dataUpdater.Update(updateEntity);
+                            string file = ASIPFolderPoolDir + "\\" + claimNo + ".zzip";
+                            string destFile = ASIPFolderPoolDir + "\\" + claimNo + ".zip";
+                            if (System.IO.File.Exists(file))
+                            {
+                                System.IO.File.Move(file, destFile);
+                                Core.Data.Entity updateEntity = new Core.Data.Entity("Reimbursement");
+                                updateEntity.SetAttributeValue("IsArchived", 1);
+                                updateEntity.SetIdValue(reimbursmentId);
+                                _dataUpdater.Update(updateEntity);
+                            }
                         }
                     }
+                }catch(Exception ex)
+                {
+                    _logService.Error(ex);
                 }
             }
 
